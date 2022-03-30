674a91bab74c549dc992bd2df05f18a5e52d1c40
Bailey Thorp <bailey.thorp@gbhs.school.nz>
2022-03-31 10:15:36 +1300

    v4.1

--- 03DTP Contacts List/Form1.cs
+++ 03DTP Contacts List/Form1.cs
@@ -13,10 +13,22 @@ namespace _03DTP_Contacts_List
 {
     public partial class Form1 : Form   
     {
+        //make it read from file on loading
+        //make a method for reading file --> listviewbox
+        //use StreamWriter?
+        string filePath = @"H:/contacts-storage.txt";
         public Form1()
         {
             InitializeComponent();
         }
+
+        public void ClearTextBoxes()
+        {
+            txtName.Text = "";
+            txtAge.Text = "";
+            txtPhone.Text = "";
+        }
+
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
@@ -25,20 +37,22 @@ namespace _03DTP_Contacts_List
             lvi.SubItems.Add(txtPhone.Text);
             listView1.Items.Add(lvi);
 
-            txtName.Text = "";
-            txtAge.Text = "";
-            txtPhone.Text = "";
+            File.WriteAllText(filePath, $"{txtName.Text},{txtAge.Text},{txtPhone.Text}");
+
+            ClearTextBoxes();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string filePath = @"H:/contacts-storage.txt";
+            listView1.Items.Clear();
+
             List<string> lines = File.ReadAllLines(filePath).ToList();
 
             foreach(string line in lines)
             {
                 string[] sections = line.Split(',');
-                //make into method
+                //make into method?
                 ListViewItem lvi = new ListViewItem(sections[0]);
                 lvi.SubItems.Add(sections[1]);
                 lvi.SubItems.Add(sections[2]);
