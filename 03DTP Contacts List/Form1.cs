797ace720e7f9ba0ff618eaf6ab8bc91875a7883
Bailey Thorp <bailey.thorp@gbhs.school.nz>
2022-06-09 10:16:31 +1200

    v9.1 Oops, all methods (incomplete)

--- 03DTP Contacts List/Form1.cs
+++ 03DTP Contacts List/Form1.cs
@@ -18,18 +18,93 @@ namespace _03DTP_Contacts_List
         public static string editAge = string.Empty;
         public static string editPhone = string.Empty;
         public static bool editConfirm = false;
+        public static int selectedIndex;
+        List<string> allLines = new List<string>();
+        public static string error = "";
+
+        public static bool validName;
+        public static bool validAge;
+        public static bool validPhone;
 
         string filePath = @"contacts-storage.txt";
 
         public frm1()
         {
             InitializeComponent();
-            if (!File.Exists(filePath)) { using (File.Create("Contacts-storage.txt")) {} }
+            CreateFileIfNeccecary();
         }
+
+
+        //
+        //Small Methods
+        //
+
+        public void CreateFileIfNeccecary()
+        {
+            if (!File.Exists(filePath)) { using (File.Create("Contacts-storage.txt")) { } }
+        }
+
+        public void ReadFile()
+        {
+            allLines = File.ReadAllLines(filePath).ToList();
+        }
+
+        public void WriteFile()
+        {
+            File.WriteAllLines(filePath, allLines);
+        }
+
+        public void ConvertFromFileToListView()
+        {
+            foreach (string line in allLines)
+            {
+                string[] sections = line.Split(',');
+
+                ListViewItem lvi = new ListViewItem(sections[0]);
+                lvi.SubItems.Add(sections[1]);
+                lvi.SubItems.Add(sections[2]);
+                listView1.Items.Add(lvi);
+            }
+        }
+
+        public void SwapLines(int a)
+        {
+            (allLines[selectedIndex], allLines[selectedIndex + a]) = (allLines[selectedIndex + a], allLines[selectedIndex]);
+        }
+
+        public void SelectNewlySwappedLine(int a)
+        {
+            listView1.Items[selectedIndex + a].Focused = true;
+            listView1.Items[selectedIndex + a].Selected = true;
+        }
+
+        public void CheckTextBoxValidity()
+        {
+            validName = Regex.IsMatch(txtName.Text, @"^[a-zA-Z\s]+$");
+            validAge = Regex.IsMatch(txtAge.Text, @"^\d+$");
+            validPhone = Regex.IsMatch(txtPhone.Text, @"^\d+$");
+        }
+        public void SetDefaultColors()
+        {
+            txtName.BackColor = SystemColors.Window;
+            txtAge.BackColor = SystemColors.Window;
+            txtPhone.BackColor = SystemColors.Window;
+        }
+        public void AppendToFile()
+        {
+            File.AppendAllText(filePath, $"{txtName.Text},{txtAge.Text},{txtPhone.Text}\n");
+        }
+        public bool AllTextBoxesAreValid()
+        {
+            bool a = (validName && validAge && validPhone);
+            return a;
+        }
+
 
         //
         //Methods
         //
+
 
         public void ClearTextBoxes()
         {
@@ -41,43 +116,28 @@ namespace _03DTP_Contacts_List
         public void LoadContacts()
         {   
             listView1.Items.Clear();
-            string[] allLines = File.ReadAllLines(filePath);
-            foreach (string line in allLines)
-            {
-                string[] sections = line.Split(',');
-
-                //put another foreach loop here
-                ListViewItem lvi = new ListViewItem(sections[0]);
-                lvi.SubItems.Add(sections[1]);
-                lvi.SubItems.Add(sections[2]);
-                listView1.Items.Add(lvi);
-            }
-        }
-
-        public void EditContact()
-        {
-            string[] allLines = File.ReadAllLines(filePath);
-            allLines[listView1.FocusedItem.Index] = $"{editName},{editAge},{editPhone}";
-            File.WriteAllLines(filePath, allLines);
+            ReadFile();
+            ConvertFromFileToListView();
         }
 
         public void Swap(int a)
         {
 
-            int selectedIndex = listView1.FocusedItem.Index;
-            string[] allLines = File.ReadAllLines(filePath);
-
-            (allLines[selectedIndex], allLines[selectedIndex + a]) = (allLines[selectedIndex + a], allLines[selectedIndex]);
-            File.WriteAllLines(filePath, allLines);
+            selectedIndex = listView1.FocusedItem.Index;
+            ReadFile();
+            SwapLines(a);
+            WriteFile();
 
             LoadContacts();
-            listView1.Items[selectedIndex + a].Focused = true;
-            listView1.Items[selectedIndex + a].Selected = true;
+            SelectNewlySwappedLine(a);
         }
+        
+
 
         //
         //Events
         //
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -86,28 +146,25 @@ namespace _03DTP_Contacts_List
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            bool validName = Regex.IsMatch(txtName.Text, @"^[a-zA-Z\s]+$");
-            bool validAge = Regex.IsMatch(txtAge.Text, @"^\d+$");
-            bool validPhone = Regex.IsMatch(txtPhone.Text, @"^\d+$");
-            string error = "";
+            CheckTextBoxValidity();
+            SetDefaultColors();
 
-            txtName.BackColor = SystemColors.Window;
-            txtAge.BackColor = SystemColors.Window;
-            txtPhone.BackColor = SystemColors.Window;
-
-            if (validName && validAge && validPhone)
+            if (AllTextBoxesAreValid())
             {
-                File.AppendAllText(filePath, $"{txtName.Text},{txtAge.Text},{txtPhone.Text}\n");
+                AppendToFile();
                 LoadContacts();
                 ClearTextBoxes();
             }
             else
             {
+                //set error message based on which textboxes are invalid
                 if (!validName) { error = error +"The name must only contain letters\n"; txtName.BackColor = Color.Pink; }
                 if (!validAge) { error = error + "The age must only contain numbers\n"; txtAge.BackColor = Color.Pink; }
                 if (!validPhone) { error = error + "The Phone No. must only contain numbers\n"; txtPhone.BackColor = Color.Pink; }
             }
             lblError.Text = error;
+            error = "";
+
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
@@ -121,7 +178,9 @@ namespace _03DTP_Contacts_List
 
             if (editConfirm)
             {
-                EditContact();
+                ReadFile();
+                allLines[listView1.FocusedItem.Index] = $"{editName},{editAge},{editPhone}";
+                WriteFile();
                 LoadContacts();
                 editConfirm = false;
             }
@@ -129,6 +188,7 @@ namespace _03DTP_Contacts_List
 
         private void btnUp_Click(object sender, EventArgs e)
         {
+            //swap selected contact with the one above it if it is not at the top
             if (listView1.FocusedItem.Index > 0)
             {
                 Swap(-1);
@@ -137,6 +197,7 @@ namespace _03DTP_Contacts_List
 
         private void btnDown_Click(object sender, EventArgs e)
         {
+            //swap selected contact with the one below if it it is not at the bottom
             if (listView1.FocusedItem.Index < listView1.Items.Count -1)
             {
                 Swap(1);
@@ -146,7 +207,7 @@ namespace _03DTP_Contacts_List
         private void btnDelete_Click(object sender, EventArgs e)
         {
             int selectedIndex = listView1.FocusedItem.Index;
-            List<string> allLines = File.ReadAllLines(filePath).ToList();
+            ReadFile();
 
             allLines.RemoveAt(selectedIndex);
 
